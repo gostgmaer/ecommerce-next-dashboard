9e75bb4190ca8c55dd4e0785a5a6599dee73496a
Kishor Sarkar <kishor.sarkar@inadev.com>
2023-11-09 17:31:53 +0530

    updated

--- src/app/dashboard/products/create/page.js
+++ src/app/dashboard/products/create/page.js
@@ -1,11 +1,22 @@
 import Dashboardlayout from "@/components/layout/dashboard/dashboard";
 import ProductForm from "@/components/pages/dashboard/products/ProductForm";
+import { baseurl } from "@/config/setting";
 import React from "react";
 
-const Page = () => {
+async function getCategories() {
+  const res = await fetch(baseurl+`/categories`)
+  const data = await res.json();
+  return data
+}
+
+
+const Page = async () => {
+
+  const category = await getCategories()
+
   return (
     <Dashboardlayout>
-      <ProductForm data={undefined} />
+      <ProductForm data={category} />
     </Dashboardlayout>
   );
 };
