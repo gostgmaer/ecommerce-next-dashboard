e472c0a87540bc34ff8b15eb674e61a3a5372bf3
Kishor Sarkar <kishor.sarkar@inadev.com>
2023-11-25 21:35:14 +0530

    updated

--- src/app/dashboard/categories/[cateID]/edit/page.js
+++ src/app/dashboard/categories/[cateID]/edit/page.js
@@ -1,11 +1,22 @@
 import Dashboardlayout from '@/components/layout/dashboard/dashboard'
 import CategoryForm from '@/components/pages/dashboard/category/CategoryForm'
+import { baseurl } from '@/config/setting';
+// import { getCategories } from '@/helper/serverCall/datafetch'
 import React from 'react'
 
+
+async function getCategories() {
+  const res = await fetch(baseurl+`/categories`)
+  const data = await res.json();
+  return data
+}
+
 const Page = () => {
+  const categories = getCategories()
+
   return (
     <Dashboardlayout>
-      <CategoryForm data={undefined} />
+        <CategoryForm />
     </Dashboardlayout>
   )
 }
