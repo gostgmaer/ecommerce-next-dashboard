f9412552d3737aa4b900d421c38f6d418ba30464
Kishor Sarkar <kishor.sarkar@inadev.com>
2024-01-21 19:47:57 +0530

    updated

--- src/app/dashboard/categories/[cateID]/edit/page.js
+++ src/app/dashboard/categories/[cateID]/edit/page.js
@@ -1,24 +1,50 @@
 import Dashboardlayout from '@/components/layout/dashboard/dashboard'
 import CategoryForm from '@/components/pages/dashboard/category/CategoryForm'
 import { baseurl } from '@/config/setting';
+import { serverMethod } from '@/helper/serverCall/datafetch';
 // import { getCategories } from '@/helper/serverCall/datafetch'
 import React from 'react'
 
 
-async function getCategories() {
-  const res = await fetch(baseurl+`/categories`)
-  const data = await res.json();
-  return data
-}
+// async function getCategories() {
+//   const res = await fetch(baseurl+`/categories`)
+//   const data = await res.json();
+//   return data
+// }
 
-const Page = () => {
-  const categories = getCategories()
+const Page = async (props) => {
+  const results = await getRecord(props.params.id)
 
   return (
     <Dashboardlayout>
-        <CategoryForm />
+        <CategoryForm data={{...results}} />
     </Dashboardlayout>
   )
 }
 
 export default Page
+
+
+
+
+
+export const getRecord = async (id)=>{
+
+  const params = {
+    method: "get",
+    header: {},
+    query: {}
+  };
+  const result = await serverMethod(
+    `/categories/${id}`,
+    params
+  );
+  const categories= await serverMethod(
+    `/categories`,
+    params
+  );
+
+
+return {result,categories}
+
+}
--- src/helper/serverCall/datafetch.js
+++ src/helper/serverCall/datafetch.js
@@ -5,3 +5,26 @@ export async function getCategories() {
     const data = await res.json();
     return data
   }
+
+
+import axios from "axios";
+
+export const serverMethod = async (endpint, params) => {
+    const option = {
+      method: params.method,
+      url: baseurl + endpint,
+      headers: {
+        ...params.header,
+      },
+      params: params?.query,
+      data: params?.data,
+    };
+    let response;
+    let error;
+    try {
+      response = await axios.request(option);
+    } catch (e) {
+      error = e.response;
+    }
+    return response?.data ? response?.data : error;
+  };
