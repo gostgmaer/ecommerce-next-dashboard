c1df76e490d935fabece02072d73793758731b90
Kishor Sarkar <kishor.sarkar@inadev.com>
2024-01-21 17:50:35 +0530

    updated

--- src/components/global/element/pagination.js
+++ src/components/global/element/pagination.js
@@ -1,86 +1,73 @@
-"use client";
-import React, { useState } from "react";
-import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+// components/Pagination.js
+import React from "react";
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
 
 const PaginatedList = ({
-  length=5,
-  options = [5, 10, 20, 50],
-  itemsPerPage,
-  setItemsPerPage,
+  totalItems,
+  limit,
   currentPage,
-  setCurrentPage,
+  setPage,
+  setLimit,
+  items = [10, 20, 50, 100],
 }) => {
-  const [items, setItems] = useState(
-    Array.from(Array(length).keys()).map((key) => key + 1)
-  );
-  const totalItems = length;
-  const totalPages = Math.ceil(totalItems / itemsPerPage);
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = Math.min(startIndex + itemsPerPage, totalItems);
-  const visibleItems = items.slice(startIndex, endIndex);
+  const totalPages = Math.ceil(totalItems / limit);
+  const startItem = (currentPage - 1) * limit + 1;
+  const endItem = Math.min(currentPage * limit, totalItems);
 
-  const goToPage = (page) => {
-    if (page >= 1 && page <= totalPages) {
-      setCurrentPage(page);
+  const handlePageChange = (newPage) => {
+    if (newPage >= 1 && newPage <= totalPages) {
+      setPage(newPage);
     }
   };
 
-  // const options = [5, 10, 20, 50];
+  const handleItemsPerPageChange = (e) => {
+    const newItemsPerPage = parseInt(e.target.value, 10);
+    setLimit(newItemsPerPage);
+  };
 
   return (
-    <nav
-      className="flex flex-col md:flex-row text-black justify-between items-start md:items-center space-y-3 md:space-y-0 py-2"
-      aria-label="Table navigation"
-    >
-      <div className="flex items-center space-x-4">
-        <span className="text-gray-600">Items per page:</span>
+    <div className="flex items-center my-2">
+      <div className=" flex-1">
+        <label className="mr-2">Items per Page:</label>
         <select
-          value={itemsPerPage}
-          onChange={(e) => setItemsPerPage(parseInt(e.target.value, 10))}
-          className="border border-gray-300 rounded-md px-2 py-1 text-gray-600 focus:outline-none focus:ring focus:border-indigo-300 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:focus:border-indigo-600"
+          value={limit}
+          onChange={handleItemsPerPageChange}
+          className="border rounded-md px-2 py-1  "
         >
-          {options.map((option) => (
-            <option key={option} value={option}>
-              {option}
+          {items.map((value) => (
+            <option key={value} value={value}>
+              {value}
             </option>
           ))}
         </select>
       </div>
-      <div className="flex items-center gap-5">
-        <p className="text-sm flex gap-2 font-normal text-gray-500 dark:text-gray-400">
-          Showing
-          <span className="font-semibold text-gray-900 ">
-            {startIndex}-{endIndex}
-          </span>
-          of
-          <span className="font-semibold text-gray-900 ">{totalItems}</span>
-        </p>
 
-        <div className="inline-flex items-stretch -space-x-px">
-          <button
-            className={`flex items-center justify-center cursor-pointer w-max h-10 py-2 px-3 ml-0 border rounded-2xl bg-gray-700 text-white hover:bg-gray-900 disabled:bg-gray-400 ${
-              currentPage === 1 ? "pointer-events-none" : ""
-            }`}
-            onClick={() => goToPage(currentPage - 1)}
-            disabled={currentPage === 1}
-          >
-            <span className="sr-only">Previous</span>
-            <MdKeyboardArrowLeft /> Previous
-          </button>
-
-          <button
-            className={`flex items-center justify-center w-max h-10  cursor-pointer py-2 px-3 border rounded-2xl bg-gray-700 text-white hover:bg-gray-900 disabled:bg-gray-400 ${
-              currentPage === totalPages ? "pointer-events-none" : ""
-            }`}
-            onClick={() => goToPage(currentPage + 1)}
-            disabled={currentPage === totalPages}
-          >
-            <span className="sr-only">Next</span>
-            Next <MdKeyboardArrowRight />
-          </button>
-        </div>
+      <div className=" ">
+        {" "}
+        <span className="mx-2">
+          Showing {startItem} to {endItem} of {totalItems}
+        </span>
+        <span className="mx-2">
+          Page {currentPage} of {totalPages}
+        </span>
       </div>
-    </nav>
+      <div className="flex gap-2">
+        <button
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+          className="ml-2  rounded-full  bg-transparent   disabled:text-gray-100 dark:disabled:text-gray-500  flex gap-2 items-center"
+        >
+          <FaArrowAltCircleLeft className="w-6 h-6" />
+        </button>
+        <button
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+          className="ml-2   rounded-full  bg-transparent  disabled:text-gray-100 dark:disabled:text-gray-500  flex gap-2 items-center"
+        >
+          <FaArrowAltCircleRight className="w-6 h-6" />
+        </button>
+      </div>
+    </div>
   );
 };
 
