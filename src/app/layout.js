f8a31e730590167135a7fa6779276d66d5cacea1
Kishor Sarkar <kishor.sarkar@inadev.com>
2024-02-24 21:25:47 +0530

    updated changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,20 +17,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <NextAuthProvider>
-    <AppProvider>
-      <AuthContextProvider>
-        <html lang="en">
-          <body className={inter.className} suppressHydrationWarning={true}>
-            <Suspense fallback={<Spinner></Spinner>}>
-              <NextTopLoader />
-              <main className="bg-light w-full min-h-screen text-dark">
-                {children}
-              </main>
-            </Suspense>
-            <ToastContainer />
-          </body>
-        </html>
-      </AuthContextProvider>
-    </AppProvider></NextAuthProvider>
+      <AppProvider>
+        <AuthContextProvider>
+          <html lang="en">
+            <body className={inter.className} suppressHydrationWarning={true}>
+              <Suspense fallback={<Spinner></Spinner>}>
+                <NextTopLoader />
+                <main className="bg-light w-full min-h-screen text-dark">
+                  {children}
+                </main>
+              </Suspense>
+              <ToastContainer />
+            </body>
+          </html>
+        </AuthContextProvider>
+      </AppProvider></NextAuthProvider>
   );
 }
